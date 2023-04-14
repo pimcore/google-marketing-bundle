79d817955984f5341bdb1cf72ccfe6020461e2d0
Martin Eiber <martin.eiber@pimcore.com>
2023-04-14 12:31:11 +0200

    Add codeception action.

--- .github/ci/files/config/bundles.php
+++ .github/ci/files/config/bundles.php
@@ -1,6 +1,5 @@
 <?php
 
 return [
-    'Pimcore\\Bundle\\EcommerceFrameworkBundle\\PimcoreEcommerceFrameworkBundle' => ['all' => true],
     'Pimcore\\Bundle\\GoogleMarketingBundle\\PimcoreGoogleMarketingBundle' => ['all' => true],
 ];
