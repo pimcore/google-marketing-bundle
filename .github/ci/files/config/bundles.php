f4c02622386ec11acdf3c7b3cc2fd81514ec465d
Martin Eiber <martin.eiber@pimcore.com>
2023-04-24 17:18:14 +0200

    Remove AdminBundle from bundle.php and update .gitignore

--- .github/ci/files/config/bundles.php
+++ .github/ci/files/config/bundles.php
@@ -1,6 +1,5 @@
 <?php
 
 return [
-    'Pimcore\\Bundle\\AdminBundle\\PimcoreAdminBundle' => ['all' => true],
     'Pimcore\\Bundle\\GoogleMarketingBundle\\PimcoreGoogleMarketingBundle' => ['all' => true],
 ];
